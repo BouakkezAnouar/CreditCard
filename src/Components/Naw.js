8f76df4e31b676c81754374d9f82e697cf130412
BouakkezAnouar <anouar.bouakkez@gmail.com>
2018-08-03 13:22:07 +0100

    first-commit

---
+++ src/Components/Naw.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { CreditCard } from "./CreditCard";
+import FormCard from "./FormCard";
+const Naw = () => {
+  return (
+    <div className="container p-5 bg-light">
+      <CreditCard />
+      <FormCard />
+    </div>
+  );
+};
+
+export default Naw;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Naw from "./Components/Naw";
+import "./js/verification";
+import $ from "jquery";
+
+ReactDOM.render(<Naw />, document.getElementById("root"));
